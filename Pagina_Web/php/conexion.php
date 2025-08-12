efb2f32ae90cc840686ffeabb36133d4a83f383d
AzulEren <al.abravo@centrogeo.edu.mx>
2025-08-12 17:43:30 -0600

    Delete Pagina_Web directory

--- Pagina_Web/php/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conexion = pg_connect("host=localhost dbname=caminabilidad port=5432 user=postgres password=ubuntu");
-
-if (!$conexion) {
-  die("No se ha podido establecer conexion con la bd.");
-}
-
-?>
